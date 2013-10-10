34106f66d404bf66c7f20cd52a48ba1b922eccbd
Vladimir Gitlevich <vlad@xaoc.com>
2013-10-10 16:58:45 -0700

    Cleanup + added file save dialog and file filtration

--- src/main/scala/com/domainlanguage/schedule/Scheduler.scala
+++ src/main/scala/com/domainlanguage/schedule/Scheduler.scala
@@ -6,58 +6,62 @@ import java.io.File
 import scala.Predef._
 import scala.swing.event.ButtonClicked
 import javax.swing.event.{TableModelEvent, TableModelListener}
+import javax.swing.filechooser.FileFilter
 
 /**
  * User: Vladimir Gitlevich
  * Date: 10/8/13
  * Time: 23:27
  */
-object Scheduler extends SimpleSwingApplication with TableModelListener {
+object Scheduler extends SimpleSwingApplication {
 
   import Conversions._
 
-  val repository = new FileBasedScheduleRepository()
-  var isScheduleChanged = false
-  var shouldSaveSchedule = true
-
-  var file: File = null
-
-  var schedule = Schedule()
-  var schedulePane = new SchedulePane(schedule2Grid(schedule, this))
-  val fileChooser = new FileChooser()
-
-  val addEntryButton = new Button("Add entry")
-  val removeEntryButton = new Button("Remove entry")
-
-  val boxPanel = new BoxPanel(Orientation.Vertical) {
-    contents += schedulePane
-    contents += addEntryButton
-    contents += removeEntryButton
-    border = Swing.EmptyBorder(30, 30, 10, 30)
-  }
-
-  val saveMenuItem = new MenuItem(Action("Save") {
-    println(schedulePane.table.model)
-    saveSchedule(file, schedulePane)
-  })
-  saveMenuItem.enabled = false
-
-  val openMenuItem = new MenuItem(Action("Open") {
-    if (fileChooser.showOpenDialog(schedulePane) == FileChooser.Result.Approve) {
-      file = fileChooser.selectedFile
-      schedule = repository.findBy(FileScheduleSpec(file))
-      schedulePane = new SchedulePane(schedule2Grid(schedule, this))
-      saveMenuItem.enabled = true
-      boxPanel.contents.update(0, schedulePane)
-      boxPanel.revalidate()
-    }
-  })
-
-
   def top = {
+    new MainFrame with TableModelListener {
+      val repository = new FileBasedScheduleRepository()
+      var scheduleFile: File = null
 
-    new MainFrame {
       title = "Domain Language Class Scheduler"
+
+      var isScheduleChanged = false
+      var shouldSaveSchedule = true
+      var schedule = Schedule()
+      var schedulePane = new SchedulePane(schedule2Grid(schedule, this))
+
+      val fileChooser = new FileChooser()
+      fileChooser.fileSelectionMode = FileChooser.SelectionMode.FilesOnly
+      fileChooser.fileFilter = new FileFilter() {
+        override def accept(f: File): Boolean = f.getName.endsWith(".json")
+        override def getDescription = "json files (.json)"
+      }
+
+      val addEntryButton = new Button("Add entry")
+      val removeEntryButton = new Button("Remove entry")
+
+      val boxPanel = new BoxPanel(Orientation.Vertical) {
+        contents += schedulePane
+        contents += addEntryButton
+        contents += removeEntryButton
+        border = Swing.EmptyBorder(30, 30, 10, 30)
+      }
+
+      val saveMenuItem = new MenuItem(Action("Save") {
+        println(schedulePane.table.model)
+        saveSchedule()
+      })
+      saveMenuItem.enabled = false
+
+      val openMenuItem = new MenuItem(Action("Open") {
+        if (fileChooser.showOpenDialog(schedulePane) == FileChooser.Result.Approve) {
+          scheduleFile = fileChooser.selectedFile
+          schedule = repository.findBy(FileScheduleSpec(scheduleFile))
+          schedulePane = new SchedulePane(schedule2Grid(schedule, this))
+          saveMenuItem.enabled = true
+          boxPanel.contents.update(0, schedulePane)
+          boxPanel.revalidate()
+        }
+      })
 
       menuBar = new MenuBar {
         contents += new Menu("File") {
@@ -66,7 +70,6 @@ object Scheduler extends SimpleSwingApplication with TableModelListener {
           contents += new Separator
           contents += new MenuItem(Action("Quit") {
             showCloseDialog()
-            println("quitting")
           })
         }
       }
@@ -79,37 +82,41 @@ object Scheduler extends SimpleSwingApplication with TableModelListener {
 
       contents = boxPanel
 
+      def tableChanged(event: TableModelEvent): Unit = {
+        println(s"Notified of table change: $event")
+        isScheduleChanged = true
+        saveMenuItem.enabled = true
+      }
+
+      def saveSchedule() {
+        if (isScheduleChanged) {
+          if (scheduleFile == null) {
+            if (fileChooser.showSaveDialog(schedulePane) == FileChooser.Result.Approve) {
+              scheduleFile = fileChooser.selectedFile
+            }
+          }
+          repository.save(scheduleFile, grid2Schedule(schedulePane.table.model.asInstanceOf[Grid]))
+          isScheduleChanged = false
+        }
+      }
+
       private def showCloseDialog() {
-        println("Showing close dialog")
+        if (!isScheduleChanged) quit()
+
         Dialog.showConfirmation(parent = null,
           title = "Exit",
           message = "Save schedule before exit?",
           optionType = Dialog.Options.YesNoCancel
         ) match {
           case Dialog.Result.Yes =>
-            println("YES")
-            saveSchedule(file, schedulePane)
-            super.closeOperation()
+            saveSchedule()
+            quit()
           case Dialog.Result.No =>
-            println("NO")
-            super.closeOperation()
+            quit()
           case _ => ()
         }
       }
     }
-  }
-
-
-  def saveSchedule(file: File, schedulePane: SchedulePane) {
-    if (file != null && isScheduleChanged) {
-      repository.save(file, grid2Schedule(schedulePane.table.model.asInstanceOf[Grid]))
-      isScheduleChanged = false
-    }
-  }
-
-  def tableChanged(event: TableModelEvent): Unit = {
-    println(s"Notified of table change: $event")
-    isScheduleChanged = true
   }
 
 }
